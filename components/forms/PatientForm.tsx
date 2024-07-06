626083c064445e335fa3ec850c833fa8b1bc7b5c
Navin grg <grgnabin60@gmail.com>
2024-07-06 12:51:09 +0545

    Added custom form field

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -2,23 +2,27 @@
 import { Button } from "@/components/ui/button"
 import {
     Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
 } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import CustomFromField from "../CustomFromField"
 
 const formSchema = z.object({
         username: z.string().min(2, {
         message: "Username must be at least 2 characters.",
     }),
 })
+
+export enum FormFieldType {
+    INPUT = 'input',
+    TEXTAREA = 'textarea',
+    PHONE_INPUT = 'phoneInput',
+    CHECKBOX = 'checkbox',
+    DATE_PICKER = 'datepicker',
+    SELECT = 'select',
+    SKELETON = 'skeleton'
+}
 
 const PatientForm = () => {
   // 1. Define your form.
@@ -37,23 +41,22 @@ const PatientForm = () => {
 }
     return (
         <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-                <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                    This is your public display name.
-                </FormDescription>
-                <FormMessage />
-                </FormItem>
-            )}
-            />
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+            <section className="mb-12 space-y-4">
+                <h1>Hi there 👋 </h1>
+                <p className="text-dark-700">Schedule your first appoinment</p>
+            </section>
+            
+            <CustomFromField 
+                control= {form.control} 
+                fieldType={FormFieldType.INPUT}
+                name="name"
+                label="Full name"
+                placeholder= "John Doe"
+                iconSrc="/asstes/icons/user.svg"
+                iconAlt="user"
+                />
+
             <Button type="submit">Submit</Button>
         </form>
     </Form>
